bf37c1e7623fb2eecf9e433335aa3c59b0e5d85b
Nikhel Krishna <nikhelkrishna@gmail.com>
2024-11-12 14:22:36 -0500

    fixed problem 1

--- CS3300CodeSmells/src/LoginService.java
+++ CS3300CodeSmells/src/LoginService.java
@@ -1,4 +1,35 @@
 public class LoginService {
+    private final LoginManager loginManager = new LoginManager();
+    private final RegistrationManager registrationManager = new RegistrationManager();
+    private final AccountManager accountManager = new AccountManager();
+
+    public void login(User user) {
+        loginManager.login(user);
+    }
+
+    public void logout(User user) {
+        loginManager.logout(user);
+    }
+
+    public void register(User user) {
+        registrationManager.register(user);
+    }
+
+    public void resetPassword(User user) {
+        accountManager.resetPassword(user);
+    }
+
+    public void suspendAccount(User user) {
+        accountManager.suspendAccount(user);
+    }
+
+    public void activateAccount(User user) {
+        accountManager.activateAccount(user);
+    }
+}
+
+// Separate class to handle login and logout operations
+class LoginManager {
     public void login(User user) {
         System.out.println("User " + user.getName() + " logged in.");
     }
@@ -6,11 +37,17 @@ public class LoginService {
     public void logout(User user) {
         System.out.println("User " + user.getName() + " logged out.");
     }
+}
 
+// Separate class to handle user registration
+class RegistrationManager {
     public void register(User user) {
         System.out.println("User " + user.getName() + " registered.");
     }
+}
 
+// Separate class to manage account actions such as resetting password, suspending, or activating account
+class AccountManager {
     public void resetPassword(User user) {
         System.out.println("Password for " + user.getName() + " reset.");
     }
@@ -22,9 +59,9 @@ public class LoginService {
     public void activateAccount(User user) {
         System.out.println("Account for " + user.getName() + " activated.");
     }
-
 }
 
+// User class with basic fields and a getter method for name
 class User {
     private String name;
     private String email;
@@ -39,5 +76,4 @@ class User {
     public String getName() {
         return name;
     }
-
 }
